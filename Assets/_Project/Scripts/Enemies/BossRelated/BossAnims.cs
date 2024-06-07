e0a39d6de22687c7a514d5ffbe75941663cce63f
COUETTE Allan <couetteallan@gmail.com>
2024-06-07 09:27:16 +0200

    last sounds

--- Assets/_Project/Scripts/Enemies/BossRelated/BossAnims.cs
+++ Assets/_Project/Scripts/Enemies/BossRelated/BossAnims.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class BossAnims : MonoBehaviour
 {
-    [SerializeField] private MMF_Player _feedback,_dash;
+    [SerializeField] private MMF_Player _feedback, _dash,_throw;
     public void PlaySoundTantrum()
     {
         _feedback.PlayFeedbacks();
     }
     public void PlaySoundDash()
     {
-        //_dash.PlayFeedbacks();
+        _dash.PlayFeedbacks();
+    }
+
+    public void PlaySoundThrow()
+    {
+        _throw.PlayFeedbacks();
     }
 }
